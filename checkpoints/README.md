6698367f60236e049c39f8a62d5edeef65fe5682
WouterBant <wouterbant01@gmail.com>
2024-09-30 19:40:11 +0200

    Script for downloading checkpoints

---
+++ checkpoints/README.md
@@ -0,0 +1,9 @@
+## checkpoints
+
+`download_checkpoints.sh` contains commands to download the various models.
+
+By default all commands are commented, uncomment the ones you are interested in and run:
+
+```bash
+bash download_checkpoints.sh
+```
---
+++ checkpoints/download_checkpoints.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Best weights for the samed like method
+# wget --header="Referer: https://huggingface.co/" -P . https://huggingface.co/Wouter01/AI4MI/resolve/main/bestweights_samed_1024_r4_lowreslogits.pt
+
+# The segment anything vit_b checkpoint (from meta)
+# wget -P ../src/samed/checkpoints https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth
