d2bbc6c5e1f2694c27e8dcb5614f7417b1c01024
WouterBant <wouterbant01@gmail.com>
2024-10-04 13:44:36 +0200

    added commands to downlaod each of the fast samed variations

--- checkpoints/download_checkpoints.sh
+++ checkpoints/download_checkpoints.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Best weights for the samed like method
-# wget --header="Referer: https://huggingface.co/" -P . https://huggingface.co/Wouter01/AI4MI/resolve/main/bestweights_samed_1024_r4_lowreslogits.pt
+# wget --header="Referer: https://huggingface.co/" -P . https://huggingface.co/Wouter01/AI4MI/resolve/main/bestweights_samed_1024_r4_augment_no_normalize_no.pt
+# wget --header="Referer: https://huggingface.co/" -P . https://huggingface.co/Wouter01/AI4MI/resolve/main/bestweights_samed_512_r6_augment_no_normalize_no.pt
+# wget --header="Referer: https://huggingface.co/" -P . https://huggingface.co/Wouter01/AI4MI/resolve/main/bestweights_samed_512_r6_augment_yes_normalize_no.pt
+# wget --header="Referer: https://huggingface.co/" -P . https://huggingface.co/Wouter01/AI4MI/resolve/main/bestweights_samed_512_r6_augment_no_normalize_yes.pt
+# wget --header="Referer: https://huggingface.co/" -P . https://huggingface.co/Wouter01/AI4MI/resolve/main/bestweights_samed_512_r6_augment_yes_normalize_yes.pt
 
 # The segment anything vit_b checkpoint (from meta)
 # wget -P ../src/samed/checkpoints https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth
